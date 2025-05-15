e04e6c1557db0b419f1ba30dd6e437f296608a90
Fluck <damienfluck516@gmail.com>
2025-05-15 17:51:42 +0000

    OMG

--- app.fluck.py
+++ app.fluck.py
@@ -4,7 +4,7 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return "Nolan n'est pas Batman !"
+    return "ola boulette !"
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5000)
